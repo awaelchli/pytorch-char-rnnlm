f2af311d5ea89dd3d9ab059a56250e8b1f75e82c
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2018-05-31 17:57:16 +0200

    make initial state a parameter that can be learnt

--- model.py
+++ model.py
@@ -15,16 +15,18 @@ class RNNModel(torch.nn.Module):
         if tie_weights:
             self.decoder.weight = self.encoder.weight
 
-        self.init_weights()
-
         self.nhid = nhid
         self.nlayers = nlayers
 
-    def init_weights(self):
-        initrange = 0.1
-        self.encoder.weight.data.uniform_(-initrange, initrange)
-        self.decoder.bias.data.fill_(0)
-        self.decoder.weight.data.uniform_(-initrange, initrange)
+        h0_params = torch.zeros(nlayers, 1, self.nhid)
+        self.h0 = torch.nn.Parameter(h0_params)
+        self.c0 = torch.nn.Parameter(h0_params.clone())
+
+        # Do not learn initial states by default
+        self.h0.requires_grad = False
+        self.c0.requires_grad = False
+
+        self.init_weights()
 
     def forward(self, input, hidden):
         emb = self.drop(self.encoder(input))
@@ -35,8 +37,17 @@ class RNNModel(torch.nn.Module):
 
         return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
 
-    def init_hidden(self, bsz):
-        weight = next(self.parameters()).data
+    def learn_hidden_state(self, learn=True):
+        self.h0.requires_grad = self.c0.requires_grad = learn
 
-        return (weight.new(self.nlayers, bsz, self.nhid).zero_(),
-                weight.new(self.nlayers, bsz, self.nhid).zero_())
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def init_hidden(self, bsz):
+        h0 = self.h0.repeat(1, bsz, 1)
+        c0 = self.c0.repeat(1, bsz, 1)
+        hidden = (h0, c0)
+        return hidden
